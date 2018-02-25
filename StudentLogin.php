827ab9a7841a81b70348b5b013432876603e5d94
ekavade <ekavade2@gmail.com>
2018-02-25 22:08:45 +0530

    Primary
    Primary

---
+++ StudentLogin.php
@@ -0,0 +1,74 @@
+<?php
+include("dbConfig.php");
+//PHP code to output the Dashboard Values
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+	</head>
+	<body background="lib.jpg">
+        <!--Header -->
+        <div class="container"> 
+            <div class="jumbotron "> 
+                <h1 class="display-2"> BOOK MY BOOKS </h1> 
+                <p class="lead"> Text here</p>
+            </div>
+        </div>
+        
+        <!--navbar-->
+        <div class="container"> 
+        <ul class="nav nav-pills">  
+            <li class="nav-item">    
+                <a class="nav-link " href="index.php">Home </a>
+            </li>   
+            <li class="nav-item">    
+                <a class="nav-link active" href="StudentLogin.php">Student Login</a>  
+            </li>  
+            <li class="nav-item">    
+                <a class="nav-link " href="FacultyLogin.php">Faculty Login</a>  
+                  </li>
+                <li class="nav-item">    
+                <a class="nav-link " href="AdminLogin.php">Admin Login</a>  
+            </li>
+          
+        </ul>
+        </div>
+        
+        <!-- Dashboard-->
+        <div class="title">Student Login</div>
+		<div class="container row justify-content-end col-4 jumbotron">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div>
+        <!-- Footer -->
+        <div class="container jumbotron bg-primary">
+            
+			Shree Rayeshwar Institute of Engineering & IT library
+		</div>
+
+        <!--JS Code-->    
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins)  -->
+        <script src="js/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+include("dbConfig.php");
+//PHP code to output the Dashboard Values
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+	</head>
+	<body background="lib.jpg">
+        <!--Header -->
+        <div class="container"> 
+            <div class="jumbotron "> 
+                <h1 class="display-2"> BOOK MY BOOKS </h1> 
+                <p class="lead"> Text here</p>
+            </div>
+        </div>
+        
+        <!--navbar-->
+        <div class="container"> 
+        <ul class="nav nav-pills">  
+            <li class="nav-item">    
+                <a class="nav-link active" href="index.php">Home </a>
+            </li>   
+            <li class="nav-item">    
+                <a class="nav-link" href="StudentLogin.php">Student Login</a>  
+            </li>  
+            <li class="nav-item">    
+                <a class="nav-link " href="FacultyLogin.php">Faculty Login</a>  
+            </li>    
+            <li class="nav-item">    
+                <a class="nav-link " href="AdminLogin.php">Admin Login</a>  
+            </li>
+            
+        </ul>
+        </div>
+        
+        <!-- Dashboard-->
+        <div class="container"> 
+        <table class="table table-dark">
+        
+            <thead>
+            <th> Dashboard</th>
+            </thead>
+            <tr> 
+                 <td >Total Student : <br> <?php echo $result['count(id)'];?></td>
+
+			     <td >Total Book : <br> <?php echo $result2['count(bookId)']; ?></td>
+
+			     <td >Total Faculty : <br> <?php echo $result3['count(id)']?></td>
+            
+            </tr>
+            <tr>
+                 <td>Total Publishers: <br> <?php echo $result4; ?></td>
+
+			     <td>Price of all books: <br> <?php echo $result5['sum(price)']; ?></td>
+
+			     <td>Books in library: <br> <?php echo $result6['count(bookId)']; ?></td>
+            </tr>
+        </table>
+        </div>
+		
+        <!-- Footer -->
+        <div class="container jumbotron bg-primary">
+            
+			Shree Rayeshwar Institute of Engineering & IT library
+		</div>
+
+        <!--JS Code-->    
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins)  -->
+        <script src="js/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
