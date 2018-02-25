0792004117b14802e0837e088797a8d58a42a4ef
ekavade <ekavade2@gmail.com>
2018-02-25 22:52:42 +0530

    Update 1
    Edited till Sunday

--- AdminLogin.php
+++ AdminLogin.php
@@ -10,59 +10,58 @@ include("dbConfig.php");
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        
+
 	</head>
 	<body background="lib.jpg">
         <!--Header -->
-        <div class="container"> 
-            <div class="jumbotron "> 
-                <h1 class="display-2"> BOOK MY BOOKS </h1> 
+        <div class="container">
+            <div class="jumbotron ">
+                <h1 class="display-2"> BOOK MY BOOKS </h1>
                 <p class="lead"> Text here</p>
             </div>
         </div>
-        
+
         <!--navbar-->
-        <div class="container"> 
-        <ul class="nav nav-pills">  
-            <li class="nav-item">    
+        <div class="container">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
                 <a class="nav-link " href="index.php">Home </a>
-            </li>   
-            <li class="nav-item">    
-                <a class="nav-link " href="StudentLogin.php">Student Login</a>  
-            </li>  
-            <li class="nav-item">    
-                <a class="nav-link " href="FacultyLogin.php">Faculty Login</a> 
-                </li>
-                <li class="nav-item">    
-                <a class="nav-link active" href="AdminLogin.php">Admin Login</a>  
             </li>
-            
+            <li class="nav-item">
+                <a class="nav-link " href="StudentLogin.php">Student Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="FacultyLogin.php">Faculty Login</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" href="AdminLogin.php">Admin Login</a>
+            </li>
+
         </ul>
         </div>
-        
+
         <!-- Dashboard-->
-        <div class="title">Admin Login</div>
-		<div class="container">
-			<form class="loginForm">
-				<div class="formInput">
-					<input type="text" name="fullname" required autofocus placeholder="Full-Name" >
-				</div>
-				<div class="formInput">
-					<input type="password" name="pwd" required autofocus placeholder="password" >
-				</div>
-					<input type="submit" name="studentLoginBtn" value="Log In" class="btnLogin">
-					<br >
-					<a class="forgetPwd" href="home.php?activity=forgetpwd&r=user">Forget Password?</a>
-			</form>
-        </div>
-		
+				<div class="row">
+				<div class="container jumbotron col-xs-6 col-md-4 ">
+
+      	<form class="form-signin">
+        	<h2 class="form-signin-heading">Admin sign in</h2>
+        	<label for="inputEmail" class="sr-only">Email address</label>
+        	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        	<label for="inputPassword" class="sr-only">Password</label>
+        	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      	</form>
+			</div>
+			</div>
+
         <!-- Footer -->
         <div class="container jumbotron bg-primary">
-            
+
 			Shree Rayeshwar Institute of Engineering & IT library
 		</div>
 
-        <!--JS Code-->    
+        <!--JS Code-->
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins)  -->
         <script src="js/jquery.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # DataLib
+
+Home (Show Navbar, Dashboard, Footer)		Page Done	
+Student Login						Page Done
+Login						Page Done
+Registration 				
+Verify Uno and Roll No		
+Add Pass			
+Student Home Page			
+Add Booking
+Search Books
+Edit Details
+Renew Book
+Faculty Login					Page Done
+Login
+Ask if Comp Lib
+Registration
+Verify Uno and Roll No
+Add Pass
+Faculty Home page
+Add Booking
+Search Books
+Edit Details
+Renew Book
+Admin Login					Page Done
+Login
+Admin Page x2 (For Comp Lib)
+Add Books (CRUD)
+Add Students (CRUD)
+Add Faculty (CRUD)
+Issue Book
+
+
